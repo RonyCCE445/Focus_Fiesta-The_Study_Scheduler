4bb9432dd2322e6694a410858966834167e223d9
ronycce445 <ckoustav7@gmail.com>
2025-05-24 11:04:23 +0530

    Initial commit
    git push origin master#	new file:   node_modules/caniuse-lite/data/features/fullscreen.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://jorvodpjbrdmuifjohtv.supabase.co';       
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpvcnZvZHBqYnJkbXVpZmpvaHR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc4OTI1ODAsImV4cCI6MjA2MzQ2ODU4MH0.EUCrCGMyWVyKuUue0FvVdazS4aV8mvdxYsnC8QG2oZs'; // Replace with your anon key
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
